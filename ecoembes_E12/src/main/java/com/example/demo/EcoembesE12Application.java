d59afc6fbdc589d25f13dd1c9adfb7f7536eea90
promero04 <p.romero@opendeusto.es>
2025-11-06 13:10:58 +0100

    Subida de proyecto Ecoembes_E12

---
+++ ecoembes_E12/src/main/java/com/example/demo/EcoembesE12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoembesE12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoembesE12Application.class, args);
+	}
+
+}
